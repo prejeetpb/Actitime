b10ea311ca3c88da21a439fda835c21944edfabb
HP <HP@LAPTOP-GQ0IL040>
2023-01-18 14:47:44 +0530

    Next Program

--- AnotherMethod.java
+++ AnotherMethod.java
@@ -4,6 +4,7 @@ public class AnotherMethod
     public static void main(String[] args)
     {
      int a[]={1,2,3,4,5,};
+     System.out.println("hello World");
         System.out.println(Arrays.toString(a));
     }
 }
